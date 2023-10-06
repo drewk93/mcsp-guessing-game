302465869f4425242138ef0f3ce326f29265092f
Andrew Kullman <agkullman@protonmail.com>
2023-10-06 10:21:31 -0800

    Feature 3 : Added math.random and guess array

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
-// Who is playing
-// Create a prompt inside a function
-// Create random number with Math.random()
-// Create a while loop in order to execute the prompt at least once 
-// Create a switch statement for  
+// Feature 3
 
-let number = 50
+let isCorrect = false;
 
-let userResponse = prompt("Guess a number between 1 & 100.")
+let min = 1
+let max = 100
+function getBetween1and100 (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
-if (number === parseInt(userResponse)){
-    alert('Good job')
+let number = getBetween1and100(min, max)
+let guessArray = []
+
+
+function checkResponse(){
+    let userResponse = parseInt(prompt("Guess a number between 1 & 100."))
+    if (number === userResponse){
+        alert(`Good job. Your guess of ${userResponse} was correct. You made ${guessArray.length} guesses.`)
+        guessArray.push(userResponse)
+        isCorrect = true;
+    }
+    else if (userResponse > number){
+        alert(`Your guess was ${userResponse}. Guess lower.`)
+        guessArray.push(userResponse)
+    }
+    else if (userResponse < number){
+        alert(`Your guess was ${userResponse}. Guess higher.`)
+        guessArray.push(userResponse)
+    }
+    else {
+        alert ('You are so wrong because your guess was not a number')
+        guessArray.push(userResponse)
+    } 
 }
-else if (parseInt(userResponse) > number){
-    alert('Guess lower')
-}
-else if (parseInt(userResponse) < number){
-    alert('Guess higher')
-}
-else {
-    alert ('You are so wrong')
-}
 
-
-
-
-console.log(userResponse)
-
+do{
+    checkResponse();
+} while(!isCorrect)
+console.log(guessArray)
 
