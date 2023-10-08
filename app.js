7044b20a80168027c3fee405ba633d876b39c060
Andrew Kullman <agkullman@protonmail.com>
2023-10-08 15:10:42 -0800

    Reworked Version, upto Feat: 7

--- app.js
+++ app.js
@@ -1,75 +1,50 @@
-// Feature 6
-
-// function checkUser(){
-//     for (let i = 0; i < userArray.length; i++){
-//    if (userArray[i] === userName)
-// }
-
-// let number = Math.floor(Math.random() * 101)
-
-// let user Obj = {}
-
-function getBetween1and100 (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-
-let number = getBetween1and100(min, max)
-let guessArray = []
-
-do{
-    startGame()
-    checkResponse();
-} while(!isCorrect)
-
-function startGame(){
-    let userArray = []
-    let userName = prompt("What is your username?")
-    userArray.push(userName);
-    let isCorrect = false;
-    let min = 1
-    let max = 3
+let userObject = {
 }
 
+play()
+function play(){
+    let currentUser = prompt('What is your username?')
 
-/*TODO 
--1 make array take in strings and not just be NaN
--2 for loop to iterate over array to check for prior guesses - see function checkRepeatedNums()
-*/
-function checkResponse(){
-    let userResponse = parseInt(prompt("Guess a number between 1 & 3."))
-    if (number === userResponse){
-        alert(`Good job ${userName}. Your guess of ${userResponse} was correct. You made ${guessArray.length} guesses.`)
-        alert(`Your previous guesses were ${guessArray}`)
-        isCorrect = true;
-        let playAgain = prompt(`Would you like to play again?. If yes, input 'yes'; if no input 'no'.` )
-        if (playAgain === 'yes'){
-            startGame()
-            checkResponse();
-            isCorrect = false;
-            guessArray = [];
-        }else if (playAgain !== 'yes' || playAgain !== 'no'){
-            alert(`There's no maybe. ${playAgain} is not a valid response dude.`)
-    
+    let guess = 0;
+    let guessArray = [];
+    let max = 100;
+    let min = 1;
+    let number = Math.floor(Math.random() * (max - min + 1) + min);
+    while (guess !== number){
+            guess = parseInt(prompt(`Guess a number between 1 & 100 + ${number}`))
+        if (typeof guess === "number"){
+            guessArray.push(guess)
+        }
+        if (guess === number){
+            alert(`Great job, ${currentUser}! Out of ${guessArray.length} guess${guessArray.length > 1 ? 'es' : ''}, your guess of ${guess} was correct.
+            \n Guesses: ${guessArray}`)
+            checkHighScore();
+            playAgain(currentUser, prompt(`Would you like to play again ${currentUser}? 'Yes' or 'No'?`))
+        }
+        else if (guess < number){
+            alert(`Guess higher ${currentUser}!`)
+        }
+        else if (guess > number){
+            alert(`Guess lower ${currentUser}!`)
         }
     }
-    else if (userResponse > number){
-        alert(`Sorry ${userName}. Your guess was ${userResponse}. Guess lower!`)
+    function playAgain (user, input) {
+        if (input === 'Yes'){
+            play();
+        }
+        else {
+            alert (`Thanks for playing ${user}!`)
+        }
     }
-    else if (userResponse < number){
-        alert(`Sorry ${userName}. Your guess was ${userResponse}. Guess higher!`)
+    function checkHighScore (){
+        for (let [key, value] of Object.entries(userObject)) {
+            if (currentUser === key && guessArray.length < value){ 
+                userObject[currentUser] = guessArray.length;
+                 alert(`Good job. You beat your high score of ${value}`)
+            }
+          }
     }
-    else {
-        alert (`What is wrong with you ${userName}!!! You are so wrong because your guess was not a number`)
-    } 
-
-    guessArray.push(userResponse)
 }
 
 
 
-// function checkRepeatedNums(guessArray)
-
-
-
-
-// console.log(guessArray)
